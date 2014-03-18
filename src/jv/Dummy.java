3cb337c2dba1f5af769ef95d07727d844480587d
Miroslav Radojević <miroslav.radojevic@gmail.com>
2014-03-18 20:29:55 +0100

    added block with array manipulation

--- src/jv/Dummy.java
+++ src/jv/Dummy.java
@@ -2,8 +2,11 @@ package jv;
 
 import ij.IJ;
 
+import java.util.Arrays;
+
 /**
  * Created by miroslav on 3/13/14.
+ * java -cp "$HOME/misc/*:$HOME/jarlib/*" jv.Dummy
  */
 public class Dummy {
 
@@ -19,6 +22,18 @@ public class Dummy {
         IJ.log("is number1 a NaN? " + Float.isNaN(number1));
         IJ.log("is number2 a NaN? " + Float.isNaN(number2));
         IJ.log("MIN_VALUE" + number3);
+
+        // array with different size vectors
+        float[][][] ar = new float[1][4][];
+        ar[0][0] = new float[5];
+        ar[0][1] = new float[2];
+        ar[0][2] = null;
+        ar[0][3] = null;
+
+        IJ.log(Arrays.toString(ar[0][0]));
+        IJ.log(Arrays.toString(ar[0][1]));
+        IJ.log(Arrays.toString(ar[0][2]));
+        IJ.log(Arrays.toString(ar[0][3]));
 
     }
 
