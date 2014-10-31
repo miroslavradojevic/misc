f1d6a1fa4329f98a6cadf9cc8b1e280cd7f9483c
Miroslav Radojević <miroslav.radojevic@gmail.com>
2014-10-31 18:35:19 +0100

    some dummy example

--- src/jv/Dummy3.java
+++ src/jv/Dummy3.java
@@ -1,5 +1,7 @@
 package jv;
 
+import java.util.Arrays;
+
 /**
  * Created by miroslav on 13-10-14.
  * java -cp "$HOME/misc/*:$HOME/jarlib/*" jv.Dummy3
@@ -32,10 +34,25 @@ public class Dummy3 {
 
         }
 
+        System.out.println("");
 
+        float arr1[][] = {{0,1}, {2,3}, {4,5}, {6,7}, {8,9}, {10,11}};
+        float arr2[][] = new float[3][2]; // just allocate
 
+        // copies an array from the specified source array
+        System.arraycopy(arr1, 0, arr2, 0, 3); // src, src_pos, dest, dest_pos, len
 
+        System.out.println("arr1: " + Arrays.toString(arr1));
+        plot2(arr1);
+        System.out.println("arr2: " + Arrays.toString(arr2));
+        plot2(arr2);
 
+    }
+
+    private static void plot2(float[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(Arrays.toString(a[i]));
+        }
     }
 
 }
